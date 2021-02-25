ddd12ee7e27f18618dc382ef809e7b47cc27002c
Eddie <eddiechen47s@gmail.com>
2021-02-25 17:06:09 +0800

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'commonExchange_iOS' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for commonExchange_iOS
+
+  pod 'SnapKit'
+  pod 'Parchment', '~> 2.4'
+  pod 'IQKeyboardManagerSwift'
+  pod 'RxSwift',    '~> 4.0'
+  pod 'RxCocoa',    '~> 4.0'
+  pod 'RAMAnimatedTabBarController'
+end
---
+++ commonExchange_iOS/Controller/WalletViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WalletViewController.swift
+//  commonExchange_iOS
+//
+//  Created by ktrade on 2021/2/25.
+//
+
+import UIKit
+
+class WalletViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
